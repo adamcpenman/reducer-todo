d73fe19b4534c5e45782be620b63a0a4c4c1cbee
Your Name <you@example.com>
2019-10-18 08:37:49 -0500

    create separate folders and js files

---
+++ reducer-todo/src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { useState, useReducer } from 'react';
+import { initialState } from '../reducers/reducer';
+
+function Main(){
+    const [tasks, setTasks] = useState();
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Main;
---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+export const intitialState = {
+    item: "workout",
+    completed: false, 
+    id: 1
+}
+
+export function reducer(state, action){
+    switch (action.type ){
+        
+    default: 
+        return state;
+    }
+}
