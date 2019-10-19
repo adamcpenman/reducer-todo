3b1c30c590af1eee948458b0110a21cbc586f306
Your Name <you@example.com>
2019-10-19 13:11:31 -0500

    mvp

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoList from './Components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Gaga</h1>
+      <h1>YOUR TODO LIST: </h1>
+      <TodoList />
     </div>
   );
 }
--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,12 +1,25 @@
-export const intitialState = {
+export const initialState = [{
     item: "workout",
     completed: false, 
     id: 1
-}
+}]
 
 export function reducer(state, action){
     switch (action.type ){
-        
+        case "ADD_ITEM":
+            return [
+                ...state, {item: action.payload, completed: false, id: Date.now()}
+            ];
+        case "TOGGLE_COMPLETED":
+            return state.map(todo => {
+                if (todo.id === action.payload) {
+                    return {...todo, completed: !todo.completed}
+                }
+                return todo
+            });
+        case "CLEAR":
+            return state.filter(todo => !todo.completed)
+
     default: 
         return state;
     }
